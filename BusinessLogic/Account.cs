29d0bcb5cb7201682d18d23345ca0b498f283131
Daniel Cazzulino <daniel@cazzulino.com>
2025-11-09 12:57:25 -0300

    Add project files.

---
+++ BusinessLogic/Account.cs
@@ -0,0 +1,89 @@
+﻿using System.Runtime.Serialization;
+using Devlooped.CloudActors;
+
+namespace TestDomain;
+
+// NOTE: there's NO references or dependencies to any Orleans concepts. Just
+// pure CloudActors interfaces and attributes.
+
+public partial record Deposit(decimal Amount) : IActorCommand;
+
+public partial record Withdraw(decimal Amount) : IActorCommand;
+
+public partial record Close(CloseReason Reason = CloseReason.Customer) : IActorCommand<decimal>;
+
+public enum CloseReason
+{
+    Customer,
+    Fraud,
+    Other
+}
+
+public partial record GetBalance() : IActorQuery<decimal>;
+
+[Actor]
+public partial class Account
+{
+    public Account(string id) => Id = id;
+
+    [IgnoreDataMember]
+    public string Id { get; }
+
+    public decimal Balance { get; private set; }
+
+    public bool IsClosed { get; private set; }
+
+    public CloseReason Reason { get; private set; }
+
+    // Showcases that operation can also be just Execute overloads 
+    //public void Execute(Deposit command)
+    //{
+    //    // validate command
+    //    Raise(new Deposited(command.Amount));
+    //}
+    //public void Execute(Withdraw command)
+    //{
+    //    // validate command
+    //    Raise(new Withdraw(command.Amount));
+    //}
+
+    // Showcases that operations can have a name that's not Execute
+    public void Deposit(Deposit command)
+    {
+        if (IsClosed)
+            throw new InvalidOperationException("Account is closed.");
+
+        // validate command
+        Balance += command.Amount;
+    }
+
+    // Showcases that operations don't have to be async
+    public void Withdraw(Withdraw command)
+    {
+        if (IsClosed)
+            throw new InvalidOperationException("Account is closed.");
+
+        if (command.Amount > Balance)
+            throw new InvalidOperationException("Insufficient funds.");
+
+        Balance -= command.Amount;
+    }
+
+    // Showcases value-returning async operation with custom name.
+    public decimal Close(Close command)
+    {
+        if (IsClosed)
+            throw new InvalidOperationException("Account is already closed.");
+
+        var final = Balance;
+
+        Balance = 0;
+        IsClosed = true;
+        Reason = command.Reason;
+
+        return final;
+    }
+
+    // Showcases a query that doesn't change state, which becomes a [ReadOnly] grain operation.
+    public decimal Query(GetBalance _) => Balance;
+}
---
+++ CloudActorsApp/Program.cs
@@ -0,0 +1,68 @@
+﻿using Azure.Data.Tables;
+using Devlooped;
+using Devlooped.CloudActors;
+using Microsoft.AspNetCore.Mvc;
+using TestDomain;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.UseOrleans(silo =>
+{
+    if (builder.Environment.IsProduction())
+    {
+        silo.UseAzureStorageClustering(options => options.TableServiceClient = new TableServiceClient(
+                builder.Configuration["App:Storage"] ??
+                builder.Configuration["AzureWebJobsStorage"] ??
+                throw new InvalidOperationException("Missing either App:Storage or AzureWebJobsStorage connection strings."))); ;
+
+        silo.AddAzureTableGrainStorageAsDefault(options => options.TableServiceClient = new TableServiceClient(
+            builder.Configuration["App:Storage"] ??
+            builder.Configuration["AzureWebJobsStorage"] ??
+            throw new InvalidOperationException("Missing either App:Storage or AzureWebJobsStorage connection strings.")));
+    }
+    else
+    {
+        silo.UseLocalhostClustering();
+        silo.AddAzureTableGrainStorageAsDefault(options => options.TableServiceClient = 
+            CloudStorageAccount.DevelopmentStorageAccount.CreateTableServiceClient());
+    }
+
+    //silo.AddStreamstoneActorStorage(opt => opt.AutoSnapshot = true);
+});
+
+builder.Services.AddSingleton(CloudStorageAccount.DevelopmentStorageAccount);
+builder.Services.AddCloudActors();
+
+var app = builder.Build();
+
+app.MapPost("/account", async (IActorBus bus) =>
+{
+    var id = Guid.CreateVersion7().ToString("N");
+    return Results.Created($"/account/{id}", new { id });
+});
+
+app.MapGet("/account/{id}", async (string id, IActorBus bus) =>
+{
+    var balance = await bus.QueryAsync($"account/{id.Trim('"')}", new GetBalance());
+    return Results.Ok(balance);
+});
+
+app.MapPost("/account/{id}", async (string id, [FromBody] decimal amount, IActorBus bus) =>
+{
+    id = id.Trim('"');
+    if (amount > 0)
+        await bus.ExecuteAsync($"account/{id}", new Deposit(amount));
+    else if (amount < 0)
+        await bus.ExecuteAsync($"account/{id}", new Withdraw(-amount));
+
+    var balance = await bus.QueryAsync($"account/{id}", new GetBalance());
+    return Results.Ok(balance);
+});
+
+app.MapDelete("/account/{id}", async (string id, IActorBus bus) =>
+{
+    var balance = await bus.ExecuteAsync($"account/{id.Trim('"')}", new Close(CloseReason.Customer));
+    return Results.Ok(balance);
+});
+
+app.Run();
